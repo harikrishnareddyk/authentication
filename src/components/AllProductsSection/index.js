3f94cc1ca79fe45a01c0bf8a3496ca8e1cfb4006
harikrishnareddyk <kallemharik@gmail.com>
2023-10-20 09:01:23 +0000

    authentication

---
+++ src/components/AllProductsSection/index.js
@@ -0,0 +1,49 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+
+import ProductCard from '../ProductCard'
+import './index.css'
+
+class AllProductsSection extends Component {
+  state = {
+    productsList: [],
+  }
+
+  componentDidMount() {
+    this.getproducts()
+  }
+
+  getproducts = async () => {
+    const url = 'http://apis.ccbp.in/products'
+    const jwtToken = Cookies.get('jwt_token')
+    console.log(jwtToken)
+    const options = {
+      method: 'GET',
+      headers: {
+        authorization: `Bearer ${jwtToken}`,
+      },
+    }
+    const response = await fetch(url, options)
+    const data = await response.json()
+  }
+
+  renderProductsList = () => {
+    const {productsList} = this.state
+    return (
+      <div>
+        <h1 className="products-list-heading">All Products</h1>
+        <ul className="products-list">
+          {productsList.map(product => (
+            <ProductCard productData={product} key={product.id} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  render() {
+    return <>{this.renderProductsList()}</>
+  }
+}
+
+export default AllProductsSection
